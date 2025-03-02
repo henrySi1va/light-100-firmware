d22938d5fa3a43684ac3a46e651e0d99e15736e9
henrySi1va <henry@wavelogix.tech>
2025-03-02 02:37:09 +0000

    Start with 2 tasks

--- app/src/main.c
+++ app/src/main.c
@@ -5,30 +5,22 @@
 #define PRIORITY 7
 #define SLEEPTIME 1
 
-/*
- * @param my_name      thread identification string
- */
-void hello_loop(const char *my_name) {
-  while (1) {
-    /* Say hello! */
-    printk("%s: Hello World from %s!\n", my_name, CONFIG_BOARD);
-
-    /* wait a while, then let other thread have a turn */
-    k_sleep(K_SECONDS(SLEEPTIME));
-  }
+void sound_thread_function(void) {
+  printk("Hello Sound\n");
+  k_sleep(K_SECONDS(5));
 }
 
-/* thread_b is a static thread spawned immediately */
-void thread_b_entry_point(void *dummy1, void *dummy2, void *dummy3) {
-  ARG_UNUSED(dummy1);
-  ARG_UNUSED(dummy2);
-  ARG_UNUSED(dummy3);
-
-  hello_loop("light-100");
+void logger_thread_function(void) {
+  k_sleep(K_SECONDS(5));
+  printk("Hello Logger\n");
 }
 
-K_THREAD_DEFINE(thread_b, STACKSIZE, thread_b_entry_point, NULL, NULL, NULL,
-                PRIORITY, 0, 100);
+K_THREAD_DEFINE(sound_thread, STACKSIZE, sound_thread_function, NULL, NULL,
+                NULL, PRIORITY, 0, 100);
+extern const k_tid_t sound_thread;
+
+K_THREAD_DEFINE(logger_thread, STACKSIZE, logger_thread_function, NULL, NULL,
+                NULL, PRIORITY, 0, 100);
 extern const k_tid_t thread_b;
 
 int main(void) { return 0; }
